902bc965cd9597282c3cac523717f466a7177f74
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2020-11-08 20:18:49 +0000

    cos tam

---
+++ app/src/main/java/com/tt/oldschoolsoccer/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tt.oldschoolsoccer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+/*
+* todo connect to firebase (picture, user, login)
+* todo add admob
+* todo activity singleplayer
+* todo activity multiplayer
+* todo login logout
+* todo user class - store only points plus name
+* todo create kratka
+* todo create field
+* todo create game logic (move, checking if next move available, check if droga na bramkę dostępna)
+* todo samouczek koniecznie !!!
+* todo puchary (rozgrywki)
+* todo nagrody za strzelone branki i wygrane mecze
+*/
