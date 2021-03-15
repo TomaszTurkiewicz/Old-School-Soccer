afb67193e48e8e1dbefcb91745343eb1ea1b9886
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2021-03-15 20:54:18 +0000

    single player back button image view

--- app/src/main/java/com/tt/oldschoolsoccer/fragments/singlePlayer/SinglePlayerEasyGameFragment.kt
+++ app/src/main/java/com/tt/oldschoolsoccer/fragments/singlePlayer/SinglePlayerEasyGameFragment.kt
@@ -883,8 +883,7 @@ class SinglePlayerEasyGameFragment : FragmentCoroutine() {
         rootView.fragment_single_player_easy_game_move_down_left_btn.background = ContextCompat.getDrawable(requireContext(),R.drawable.down_left)
 
 
-        rootView.fragment_single_player_easy_back_button.background = ButtonDrawable(requireContext(), (4*screenUnit).toDouble(), (2*screenUnit).toDouble(), screenUnit.toDouble())
-        rootView.fragment_single_player_easy_back_button.setTextSize(TypedValue.COMPLEX_UNIT_PX,screenUnit.toFloat())
+        rootView.fragment_single_player_easy_back_button.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.close))
     }
 
     private fun setConstraintLayout() {
@@ -924,16 +923,16 @@ class SinglePlayerEasyGameFragment : FragmentCoroutine() {
         set.connect(rootView.fragment_single_player_easy_game_move_up_left_btn.id, ConstraintSet.BOTTOM,rootView.fragment_single_player_easy_game_middle.id, ConstraintSet.TOP,3*screenUnit)
         set.connect(rootView.fragment_single_player_easy_game_move_up_left_btn.id, ConstraintSet.RIGHT,rootView.fragment_single_player_easy_game_middle.id, ConstraintSet.LEFT,3*screenUnit)
 
-        set.connect(rootView.fragment_single_player_easy_back_button.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_game_layout.id,ConstraintSet.TOP,2*screenUnit)
-        set.connect(rootView.fragment_single_player_easy_back_button.id,ConstraintSet.LEFT,rootView.fragment_single_player_easy_game_layout.id,ConstraintSet.LEFT,14*screenUnit)
+        set.connect(rootView.fragment_single_player_easy_back_button.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_game_layout.id,ConstraintSet.TOP,screenUnit)
+        set.connect(rootView.fragment_single_player_easy_back_button.id,ConstraintSet.LEFT,rootView.fragment_single_player_easy_game_layout.id,ConstraintSet.LEFT,16*screenUnit)
 
-        set.connect(rootView.fragment_single_player_easy_phone_icon.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_game_field.id,ConstraintSet.TOP,screenUnit)
-        set.connect(rootView.fragment_single_player_easy_phone_icon.id,ConstraintSet.LEFT,rootView.fragment_single_player_easy_game_field.id,ConstraintSet.RIGHT,2*screenUnit)
+        set.connect(rootView.fragment_single_player_easy_phone_icon.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_game_field.id,ConstraintSet.TOP,2*screenUnit)
+        set.connect(rootView.fragment_single_player_easy_phone_icon.id,ConstraintSet.LEFT,rootView.fragment_single_player_easy_game_field.id,ConstraintSet.RIGHT,screenUnit)
 
-        set.connect(rootView.fragment_single_player_easy_vs_tv.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_phone_icon.id,ConstraintSet.BOTTOM,screenUnit)
+        set.connect(rootView.fragment_single_player_easy_vs_tv.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_phone_icon.id,ConstraintSet.BOTTOM, (0.5*screenUnit).toInt())
         set.connect(rootView.fragment_single_player_easy_vs_tv.id,ConstraintSet.LEFT,rootView.fragment_single_player_easy_phone_icon.id,ConstraintSet.LEFT,0)
 
-        set.connect(rootView.fragment_single_player_easy_user_icon.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_vs_tv.id,ConstraintSet.BOTTOM,screenUnit)
+        set.connect(rootView.fragment_single_player_easy_user_icon.id,ConstraintSet.TOP,rootView.fragment_single_player_easy_vs_tv.id,ConstraintSet.BOTTOM,(0.5*screenUnit).toInt())
         set.connect(rootView.fragment_single_player_easy_user_icon.id,ConstraintSet.LEFT,rootView.fragment_single_player_easy_vs_tv.id,ConstraintSet.LEFT,0)
 
         set.applyTo(rootView.fragment_single_player_easy_game_layout)
@@ -951,7 +950,7 @@ class SinglePlayerEasyGameFragment : FragmentCoroutine() {
         rootView.fragment_single_player_easy_game_move_left_btn.layoutParams = ConstraintLayout.LayoutParams(2*screenUnit,2*screenUnit)
         rootView.fragment_single_player_easy_game_move_up_left_btn.layoutParams = ConstraintLayout.LayoutParams(2*screenUnit,2*screenUnit)
         rootView.fragment_single_player_easy_game_middle.layoutParams = ConstraintLayout.LayoutParams(2*screenUnit,2*screenUnit)
-        rootView.fragment_single_player_easy_back_button.layoutParams = ConstraintLayout.LayoutParams(4*screenUnit,2*screenUnit)
+        rootView.fragment_single_player_easy_back_button.layoutParams = ConstraintLayout.LayoutParams(2*screenUnit,2*screenUnit)
 
         rootView.fragment_single_player_easy_user_icon.layoutParams = ConstraintLayout.LayoutParams(3*screenUnit,3*screenUnit)
         rootView.fragment_single_player_easy_phone_icon.layoutParams = ConstraintLayout.LayoutParams(3*screenUnit,3*screenUnit)
