e6ef68e5790ec17b4b827fd60c0f8e8996342216
tt <tturkiewicz83@gmail.com>
2021-03-27 16:08:07 +0000

    reading users from firebase ranking fragment

--- app/src/main/java/com/tt/oldschoolsoccer/fragments/RankingFragment.kt
+++ app/src/main/java/com/tt/oldschoolsoccer/fragments/RankingFragment.kt
@@ -9,10 +9,16 @@ import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.tt.oldschoolsoccer.R
 import com.tt.oldschoolsoccer.classes.FragmentCoroutine
 import com.tt.oldschoolsoccer.classes.Functions
 import com.tt.oldschoolsoccer.classes.LoggedInStatus
+import com.tt.oldschoolsoccer.classes.UserRanking
 import com.tt.oldschoolsoccer.database.UserDB
 import com.tt.oldschoolsoccer.drawable.TileDrawable
 import kotlinx.android.synthetic.main.fragment_choose_game_type.view.*
@@ -23,9 +29,9 @@ class RankingFragment : FragmentCoroutine() {
 
     private var screenUnit = 0
     private var loggedInStatus = LoggedInStatus()
-    private lateinit var userDB: UserDB
     private lateinit var rootView: View
     private var iconSize = 0
+    private lateinit var userList: MutableList<UserRanking>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +39,8 @@ class RankingFragment : FragmentCoroutine() {
 
         screenUnit = Functions.readScreenUnit(requireContext())
         loggedInStatus = Functions.readLoggedStateFromSharedPreferences(requireContext())
+        userList = mutableListOf()
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -50,9 +58,34 @@ class RankingFragment : FragmentCoroutine() {
         setBackgroundGrid()
         setSizes()
         setButtonsUI()
-        setViewForLoggedInNotLoggedIn()
         makeConstraintLayout()
+        prepareRecyclerView()
         setOnClickListeners()
+    }
+
+    private fun prepareRecyclerView() {
+        val dbRankingRef = Firebase.database.getReference("Ranking")
+        dbRankingRef.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(user in snapshot.children){
+                        val tUser = user.getValue(UserRanking::class.java)
+                        userList.add(tUser!!)
+                    }
+                    sortAndDisplay()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //do nothing
+            }
+        })
+
+    }
+
+    private fun sortAndDisplay() {
+        //todo finish here first!!!
+
     }
 
     private fun setOnClickListeners() {
@@ -74,12 +107,13 @@ class RankingFragment : FragmentCoroutine() {
         set.connect(rootView.fragment_ranking_back_button.id,ConstraintSet.TOP,rootView.fragment_ranking_layout.id,ConstraintSet.TOP,screenUnit)
         set.connect(rootView.fragment_ranking_back_button.id,ConstraintSet.LEFT,rootView.fragment_ranking_layout.id,ConstraintSet.LEFT,16*screenUnit)
 
+        set.connect(rootView.fragment_ranking_recycler_view.id,ConstraintSet.TOP,rootView.fragment_ranking_layout.id,ConstraintSet.TOP,0)
+        set.connect(rootView.fragment_ranking_recycler_view.id,ConstraintSet.LEFT,rootView.fragment_ranking_layout.id,ConstraintSet.LEFT,0)
+
         set.applyTo(rootView.fragment_ranking_layout)
     }
 
-    private fun setViewForLoggedInNotLoggedIn() {
 
-    }
 
     private fun setButtonsUI() {
         rootView.fragment_ranking_back_button.layoutParams = ConstraintLayout.LayoutParams(iconSize,iconSize)
